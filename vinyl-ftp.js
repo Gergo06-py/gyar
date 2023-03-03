6cafae31f931b7b16f0652b84dd9ae131ab0090f
Borbély Gergő Árpád <borbelygergo06@gmail.com>
2023-03-03 10:06:31 +0100

    Changed gitignore and added config dir and file

--- vinyl-ftp.js
+++ vinyl-ftp.js
@@ -1,10 +1,12 @@
 var fs = require("vinyl-fs");
 var ftp = require("vinyl-ftp");
+var reader = require("properties-reader");
+var config = reader("config/config.properties");
 
 var conn = new ftp({
-  host: "ftpupload.net",
-  user: ".",
-  password: ".",
+  host: config.get("hosting.host"),
+  user: config.get("hosting.user"),
+  password: config.get("hosting.pass"),
   parallel: 10,
   secure: true,
   secureOptions: { rejectUnauthorized: false },
