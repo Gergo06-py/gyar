8a16d5aa798fe339fcbf049c4f962d39454fe00e
Borbély Gergő Árpád <borbelygergo06@gmail.com>
2023-03-03 10:11:12 +0100

    Changed gitignore

--- vinyl-ftp.js
+++ vinyl-ftp.js
@@ -3,10 +3,14 @@ var ftp = require("vinyl-ftp");
 var reader = require("properties-reader");
 var config = reader("config/config.properties");
 
+const host = config.get("hosting.host");
+const user = config.get("hosting.user");
+const pass = config.get("hosting.pass");
+
 var conn = new ftp({
-  host: config.get("hosting.host"),
-  user: config.get("hosting.user"),
-  password: config.get("hosting.pass"),
+  host: host,
+  user: user,
+  password: pass,
   parallel: 10,
   secure: true,
   secureOptions: { rejectUnauthorized: false },
